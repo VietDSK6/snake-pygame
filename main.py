e176c702763a21b06267b45a705b0e9c0e3fa0a9
VietDSK6 <vietm1a4@gmail.com>
2024-12-19 13:45:41 +0700

    clean

--- main.py
+++ main.py
@@ -10,31 +10,32 @@ def main():
     # Khởi tạo cửa sổ game với cài đặt xác định
     game_window = pygame.display.set_mode((FRAME_SIZE_X, FRAME_SIZE_Y))
     pygame.display.set_caption('Sizzle Sizzle')
-    pygame.display.set_mode((FRAME_SIZE_X, FRAME_SIZE_Y),
-                            pygame.RESIZABLE | pygame.HWSURFACE | pygame.DOUBLEBUF)
+    pygame.display.set_mode((FRAME_SIZE_X, FRAME_SIZE_Y), pygame.RESIZABLE)
 
     # Khởi tạo và bắt đầu nhạc nền
     pygame.mixer.music.set_volume(0.5)  # Đặt âm lượng 50%
-    pygame.mixer.music.play(-1)  # Phát trong vòng lặp vô hạn
+    pygame.mixer.music.play(1)  # Phát trong vòng lặp vô hạn
 
     difficulty = "Medium"  # Đặt độ khó mặc định
 
     # Vòng lặp chương trình chính
     while True:
         # Hiển thị menu chính và nhận lựa chọn người chơi
-        choice = main_menu(game_window)
-        if choice[0] == "Quit":
+        lst = main_menu(game_window)
+        choice = lst[0]
+        color = lst[1]
+        if choice == "Quit":
             pygame.mixer.music.stop()  # Dừng nhạc trước khi thoát
             break
 
         # Xử lý chuyển đổi trạng thái game
-        while choice[0] == "Start Game" or choice[0] == "Play Again" or choice[0] == "Change Difficulty":
+        while choice == "Start Game" or choice == "Play Again":
             # Hiển thị menu độ khó nếu bắt đầu game mới hoặc thay đổi độ khó
-            if choice[0] == "Start Game" or choice[0] == "Change Difficulty":
+            if choice == "Start Game":
                 difficulty = difficulty_menu(game_window)
 
             # Bắt đầu vòng lặp game với độ khó đã chọn
-            choice = game_loop(game_window, difficulty, choice[1])
+            choice = game_loop(game_window, difficulty, color)
             if choice == "Quit":
                 pygame.mixer.music.stop()  # Dừng nhạc trước khi thoát
                 pygame.quit()
